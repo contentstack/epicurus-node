0aade77ca6c8993c674c75277ab5191843f2eaa2
Dmytro <d.mona22@gmail.com>
2021-05-05 10:23:44 +0300

    AUTH-75. Fix of issue when redis is restarted after shutting down. Two new methods for getting clients.

---
+++ js/index.d.ts
@@ -0,0 +1,13 @@
+import { EpicurusRedisConfig, serverCallback, subscribeCallback } from './interface';
+import { RedisClient } from 'redis';
+export default function Epicurus(redisConfig?: EpicurusRedisConfig): EpicurusPublicInterface;
+export declare type EpicurusPublicInterface = {
+    getRedisClient: () => RedisClient;
+    getRedisSubClient: () => RedisClient;
+    subscribe: <T = any>(channel: string, callback: subscribeCallback<T>) => Promise<void>;
+    publish: (channel: string, body: any) => void;
+    server: <T = any, S = any>(channel: string, callback: serverCallback<T, S>) => Promise<void>;
+    request: <T = any>(channel: string, body: any) => Promise<T>;
+    shutdown: () => void;
+    close: () => void;
+};
